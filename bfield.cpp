a0d26a7e1707e258d4a861982e319b7cd425bd0f
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-30 19:43:55 +0100

    Check reserved part values fit the width

--- bfield.cpp
+++ bfield.cpp
@@ -1,11 +1,15 @@
 #include "bfield.hpp"
 
+#include <cassert>
+
 BPart::BPart(const std::string &name, unsigned width)
     : m_name{name}, m_width{width} {
+
 }
 
 BPart::BPart(unsigned width, unsigned reserved)
     : m_name{std::nullopt}, m_width{width}, m_reserved_value{reserved} {
+    assert(!in_bit_width(reserved, width));
 }
 
 const std::string &BPart::name() const {
@@ -28,6 +32,7 @@ bool BPart::reserved() const {
 BField::BField(const std::string &name, unsigned width,
                const std::vector<BPart> &parts)
     : m_name{name}, m_width{width}, m_parts{parts} {
+    // TODO: Check parts equal the width
 }
 
 const std::string &BField::name() const {
--- bfield.hpp
+++ bfield.hpp
@@ -4,6 +4,14 @@
 #include <vector>
 #include <optional>
 
+template<typename T> T bit(T n) {
+    return (static_cast<T>(1) << n);
+}
+
+template <typename T> bool in_bit_width(T value, T bit_width) {
+    return (value & ~(bit(bit_width) - 1)) == 0;
+}
+
 class BPart {
     const std::optional<std::string> m_name;
     const unsigned m_width;

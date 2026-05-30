1f1b21c7757f99a18a689d92a79c713e8473f95b
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-30 21:17:46 +0100

    Create header and source files in generator

--- c_generator.cpp
+++ c_generator.cpp
@@ -1,5 +1,39 @@
 #include "c_generator.hpp"
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+static std::string file_from_filepath(const std::filesystem::path path) {
+    return path.filename();
+}
+
 bool generate_fields(const std::string output_basepath, std::vector<BField> fields) {
-    return false;
+    std::string source_path = output_basepath + ".c";
+    std::string header_path = output_basepath + ".h";
+    std::string header_file = file_from_filepath(header_path);
+
+    // TODO: Check if exist and only overwrite if there is a comment at the top "// AUTOGEN - DO NOT EDIT"
+    std::fstream source(source_path, std::ios_base::out);
+    std::fstream header(header_path, std::ios_base::out);
+    bool open_error{false};
+
+    if (!source.is_open()) {
+        std::cerr << "Failed to open " << source_path << '\n';
+        open_error = true;
+    }
+
+    if (!header.is_open()) {
+        std::cerr << "Failed to open " << header_path << '\n';
+        open_error = true;
+    }
+
+    if (open_error) {
+        return false;
+    }
+
+    header << "#pragma once\n\n";
+    source << "#include \"" << header_file << "\"\n\n";
+
+    return true;
 }

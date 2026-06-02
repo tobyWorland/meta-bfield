581f996c1907ee77c2f11a470ed523454cf07a9d
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-02 21:05:19 +0100

    Add BExport class

--- bfield.cpp
+++ bfield.cpp
@@ -62,9 +62,41 @@ unsigned BPart::reserved_value() const {
     return m_reserved_value;
 }
 
+BExport::BExport(const std::string &passthrough_part_name)
+    : m_name{passthrough_part_name}, m_part_names{}, m_signed{} {
+    assert(!m_name.empty());
+}
+BExport::BExport(const std::string &name, const std::vector<std::string> &part_names, bool export_signed)
+    : m_name{name}, m_part_names{part_names}, m_signed{export_signed} {
+    assert(!m_name.empty());
+    assert(!m_part_names.empty());
+}
+
+const std::string &BExport::name() const {
+    return m_name;
+}
+const std::vector<std::string> BExport::part_names() const {
+    assert(!is_passthrough());
+    return m_part_names;
+}
+bool BExport::is_passthrough() const {
+    return m_part_names.empty();
+}
+bool BExport::is_signed() const {
+    return m_signed;
+}
+
 BField::BField(const std::string &name, unsigned width,
-               const std::vector<BPart> &parts)
-    : m_name{name}, m_width{width}, m_parts{parts} {
+               const std::vector<BPart> &parts,
+               const std::vector<BExport> &exports)
+    : m_name{name}, m_width{width}, m_parts{parts}, m_exports{exports} {
+
+    if (m_exports.empty()) {
+        // No exports - export all parts
+        for (const auto &part : m_parts) {
+            m_exports.push_back(BExport(part.name()));
+        }
+    }
 
     unsigned part_width_sum{0};
     for (const BPart &part : m_parts) {
@@ -99,7 +131,23 @@ unsigned BField::reserved_value() const {
 }
 
 bool BField::any_variable_parts() const {
+    // TODO: Could be !m_exports.empty()?
     return std::any_of(m_parts.cbegin(), m_parts.cend(),
                        [](auto &part) { return !part.is_reserved(); }
         );
+}
+
+const std::vector<BExport> BField::exports() const {
+    return m_exports;
+}
+
+// TODO: Reference to BField should be included in BExport instead
+const BPart &BField::get_passthrough_part(const BExport &exp) const {
+    for (const BPart &part : m_parts) {
+        if (part.name() == exp.name()) {
+            return part;
+        }
+    }
+    assert(false);
+    std::terminate();
 }
--- bfield.hpp
+++ bfield.hpp
@@ -35,16 +35,37 @@ public:
     unsigned reserved_value() const;
 };
 
+class BExport {
+    std::string m_name;
+    std::vector<std::string> m_part_names;
+    bool m_signed;
+
+public:
+    BExport(const std::string &passthrough_part_name);
+    BExport(const std::string &name, const std::vector<std::string> &part_names, bool is_signed);
+
+    const std::string &name() const;
+    const std::vector<std::string> part_names() const;
+    bool is_passthrough() const;
+    bool is_signed() const;
+};
+
 class BField {
     const std::string m_name;
     const unsigned m_width;
     std::vector<BPart> m_parts;
-public:
-    BField(const std::string &name, unsigned width, const std::vector<BPart> &parts);
+    std::vector<BExport> m_exports;
+
+  public:
+    BField(const std::string &name, unsigned width,
+           const std::vector<BPart> &parts,
+           const std::vector<BExport> &exports = {});
 
     const std::string &name() const;
     unsigned width() const;
     const std::vector<BPart> parts() const;
     unsigned reserved_value() const; // TODO: Test
     bool any_variable_parts() const;
+    const std::vector<BExport> exports() const;
+    const BPart &get_passthrough_part(const BExport &exp) const;
 };

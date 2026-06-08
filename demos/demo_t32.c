b03647661c900213e36ab742ae9bef3daaef8047
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-08 22:14:30 +0100

    Add writing of data out to temporary file in demo_t32

--- demos/demo_t32.c
+++ demos/demo_t32.c
@@ -1,58 +1,59 @@
-#include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "t32_encoding.h"
 
+FILE* outfile = NULL;
+const char *outfilepath = "/tmp/out.bin";
+
+// Output can be checked with:
+// arm-none-eabi-objdump -b binary -m armv7e-m --disassembler-options=force-thumb -D /tmp/out.bin
+static void out(void *p, size_t n) {
+    if (outfile) {
+        fwrite(p, n, 1, outfile);
+    }
+}
+
 int main() {
 #if 1
-    uint16_t bx_lr = 0x4770;
-	struct bx_t1_parts bx_t1_parts;
-
-    printf("Match? %d\n", match_bx_t1(bx_lr));
-
-	bx_t1_parts = decode_bx_t1(bx_lr);
-	printf("Rm: %u\n", bx_t1_parts.Rm);
-
-	uint16_t bx_lr_out;
-	unsigned bits_encoded = encode_bx_t1(&bx_lr_out, &bx_t1_parts);
-	printf("Bits encoded: %u\nOut: 0x%04X\n", bits_encoded, bx_lr_out);
-
-	bx_lr_out = 0; // Clear
-	bx_t1_parts.Rm = 20; // invalid Rm
-	bits_encoded = encode_bx_t1(&bx_lr_out, &bx_t1_parts);
-	printf("Bits encoded: %u\nOut: 0x%04X\n", bits_encoded, bx_lr_out);
-
-	uint16_t nop[2];
-	encode_nop_t2((uint32_t*)nop);
-	printf("NOP.W: %04X %04X\n", nop[0], nop[1]);
-
-	uint16_t movwt3[2];
-	struct movw_i_t3_parts movw_i_t3_parts = {
-		.Rd = 4,
-		.imm16 = 21
-	};
-	encode_movw_i_t3((uint32_t*)movwt3, &movw_i_t3_parts);
-	printf("MOVW: %04X %04X\n", movwt3[0], movwt3[1]);
-
-	memset(&movw_i_t3_parts, '\0', sizeof(movw_i_t3_parts));
-	printf("Should both be zero: %u %u\n", movw_i_t3_parts.Rd, movw_i_t3_parts.imm16);
-
-	movw_i_t3_parts = decode_movw_i_t3(*(uint32_t*)movwt3);
-	printf("Rd: %u imm16: %u\n", movw_i_t3_parts.Rd, movw_i_t3_parts.imm16);
+    outfile = fopen(outfilepath, "wb");
+    if (!outfile) {
+        perror("fopen");
+        exit(1);
+    }
 #endif
 
 #if 0
-	struct b_cond_t3_noit_parts bt3 = {};
-	bt3.cond = 0;
-	bt3.simm21 = -2;
-	printf("Using: cond %u simm21 %d\n", bt3.cond, bt3.simm21);
+    uint16_t bx_lr = 0x4770;
+    struct bx_t1_parts bx_t1_parts;
 
-	uint16_t bt3out[2];
-	printf("B.W t3 Success? %u\n", encode_b_cond_t3_noit((uint32_t*)bt3out, &bt3));
-	printf("bt3 %04X %04X\n", bt3out[0], bt3out[1]);
-	bt3 = (struct b_cond_t3_noit_parts){};
-	bt3 = decode_b_cond_t3_noit(*(uint32_t*)bt3out);
-	printf("Got: cond %u simm21 %d\n", bt3.cond, bt3.simm21);
+    printf("Match? %d\n", match_bx_t1(bx_lr));
+
+    bx_t1_parts = decode_bx_t1(bx_lr);
+    printf("Rm: %u\n", bx_t1_parts.Rm);
+
+    uint16_t bx_lr_out;
+    unsigned bits_encoded = encode_bx_t1(&bx_lr_out, &bx_t1_parts);
+    printf("Bits encoded: %u\nOut: 0x%04X\n", bits_encoded, bx_lr_out);
+#endif
+
+#if 1
+    struct b_cond_t3_noit_parts bt3 = {};
+    bt3.cond = 0;
+    bt3.simm21 = -2;
+    printf("Using: cond %u simm21 %d\n", bt3.cond, bt3.simm21);
+
+    uint16_t bt3out[2];
+    printf("B.W t3 Success? %u\n", encode_b_cond_t3_noit((uint32_t*)bt3out, &bt3));
+    printf("bt3 %04X %04X\n", bt3out[0], bt3out[1]);
+
+    bt3 = (struct b_cond_t3_noit_parts){};
+    bt3 = decode_b_cond_t3_noit(*(uint32_t*)bt3out);
+    printf("Got: cond %u simm21 %d\n", bt3.cond, bt3.simm21);
+
+    out(bt3out, sizeof(bt3out));
 #endif
 
     return 0;

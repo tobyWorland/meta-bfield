d4d2eca2c80a2ae20a3b05f83d066e1a7ff4dd79
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-08 20:05:43 +0100

    Add tests for "onetwothree" from example spec

--- tests/test_c_example.cpp
+++ tests/test_c_example.cpp
@@ -19,3 +19,22 @@ TEST(example_spec, two_int8s) {
     EXPECT_EQ(encode_two_int8s(&y, &two_int8s_parts), 16);
     EXPECT_EQ(y, 0b1111'1110'0000'0100);
 }
+
+TEST(example_sec, onetwothree) {
+    onetwothree_parts ott_parts;
+
+    ott_parts.one = 1;
+    ott_parts.two = 2;
+    ott_parts.three = 3;
+
+    uint16_t expected = (2 << 8) | (4 << 4) | 6;
+    uint16_t actual;
+
+    EXPECT_EQ(encode_onetwothree(&actual, &ott_parts), 12);
+    EXPECT_EQ(actual, expected);
+
+    ott_parts = decode_onetwothree(0);
+    EXPECT_EQ(ott_parts.one, 0b1111);
+    EXPECT_EQ(ott_parts.two, 0b1110);
+    EXPECT_EQ(ott_parts.three, 0b1101);
+}

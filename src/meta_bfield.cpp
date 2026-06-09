9efc214f26a239468ec631c3cdd23d8d179e5737
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-09 02:07:41 +0100

    Remove old unused dump_field function

--- src/meta_bfield.cpp
+++ src/meta_bfield.cpp
@@ -5,17 +5,6 @@
 #include "bfield.hpp"
 #include "spec.hpp"
 
-static void dump_field(const BField &field) {
-    std::cout << field.name() << " " << field.width() << '\n';
-    for (auto &part : field.parts()) {
-        std::cout << part->name() << " " << part->width();
-        if (part->is_reserved()) {
-            std::cout << " 0x" << std::hex << part->reserved_value();
-        }
-        std::cout << '\n';
-    }
-}
-
 int main(int argc, char **argv) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <spec> <outpath>\n";
@@ -27,12 +16,6 @@ int main(int argc, char **argv) {
 
     SpecReader spec_reader;
     auto fields = spec_reader.read_from_spec(argv[1]);
-
-#if 0
-    for (const auto &field: fields) {
-        dump_field(field);
-    }
-#endif
 
     if (generate_fields(output_path, fields)) {
         std::cout << "Generate successful\n";
